b9195102f7f19f73b14f44feb0e165bde8ffaf72
royfrankly <ryfrnkl_nvrr@outlook.es>
2024-01-18 20:58:34 -0500

    se agrega el archivo de programas estructurados

---
+++ estructurado/primero.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"este el primer programa estructurado"<<endl;
+    return 0;
+}
+
+
---
+++ estructurado/segundo.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"este es el segundo programa estructurado"<<endl;
+
+
+    return 0;
+}
