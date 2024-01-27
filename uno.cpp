51a36195d64035cb352a197b05dc7a7a379a9bc4
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-26 22:24:13 -0500

    add funcionalidad multiplicar

--- uno.cpp
+++ uno.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 #include <string>
 #include "mostrar.h"
+#include "fmultiplicar.h"
 int main(){
     cout<<"hola mundo, tiene una nueva modificacion"<<endl;
     mostrar m1("esta es la descripcion del primer m1");
     m1.ver();
+    multiplicar(m1, 10);
     return 0;
 }
