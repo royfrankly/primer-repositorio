f2d8718227a1b8237a98621ff267e4d5e9b4f16d
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-26 21:48:03 -0500

    add funcion menu

--- uno.cpp
+++ uno.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
+void menu();
 int main(){
     cout<<"hola mundo, tiene una nueva modificacion"<<endl;
 
     return 0;
+}
+void menu(){
+    cout<<"mostrar menu"<<endl;
 }
