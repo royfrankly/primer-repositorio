f011732e0a3ad83d15e7215809ab15b869151f48
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-26 22:16:49 -0500

    add function.h

---
+++ fmultiplicar.h
@@ -0,0 +1,15 @@
+#ifndef MULTIPLICAR_H
+#define MULTIPLICAR_H
+#include <iostream>
+using namespace std;
+void multiplicar(string des, int n){
+    for(int i=0; i<n; i++){
+        cout<<des<<endl;
+    }
+}
+
+
+
+
+
+#endif
