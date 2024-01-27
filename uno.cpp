d6ad343fea69389eb0537b97de9a307bc0e38560
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-26 22:04:30 -0500

    add obj m1

--- uno.cpp
+++ uno.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
-
+#include <string>
+#include "mostrar.h"
 int main(){
     cout<<"hola mundo, tiene una nueva modificacion"<<endl;
-
+    mostrar m1("esta es la descripcion del primer m1");
+    m1.ver();
     return 0;
 }
