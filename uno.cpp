578a3f22a730cb5e8261686343b2fae941215137
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-26 21:50:54 -0500

    remove funcion menu

--- uno.cpp
+++ uno.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
-void menu();
+
 int main(){
     cout<<"hola mundo, tiene una nueva modificacion"<<endl;
 
     return 0;
-}
-void menu(){
-    cout<<"mostrar menu"<<endl;
 }
